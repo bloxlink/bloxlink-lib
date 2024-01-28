8ffaca909c5355c48f78086b305b8d8e65cdd60b
Julien Kmec <me@julien.dev>
2024-01-28 18:45:46 -0500

    use attrs dataclass instead

--- bot_utils/config.py
+++ bot_utils/config.py
@@ -1,18 +1,19 @@
-from typing import TypedDict, NotRequired
+from attrs import define
 from dotenv import dotenv_values
 
 
-class Config(TypedDict):
+@define(slots=True, kw_only=True)
+class Config:
     """Type definition for config values."""
 
     #############################
     MONGO_URL: str
-    MONGO_CA_FILE: NotRequired[str]
+    MONGO_CA_FILE: str = None
     # these are optional because we can choose to use REDIS_URL or REDIS_HOST/REDIS_PORT/REDIS_PASSWORD
-    REDIS_URL: NotRequired[str]
-    REDIS_HOST: NotRequired[str]
-    REDIS_PORT: NotRequired[str]
-    REDIS_PASSWORD: NotRequired[str]
+    REDIS_URL: str = None
+    REDIS_HOST: str = None
+    REDIS_PORT: str = None
+    REDIS_PASSWORD: str = None
 
 
 CONFIG: Config = Config(**dotenv_values())

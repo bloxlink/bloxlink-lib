70d5e75e1230c137ad12465957f8affc1c7e616d
Julien Kmec <me@julien.dev>
2024-01-28 23:23:56 -0500

    export groups

--- bot_utils/__init__.py
+++ bot_utils/__init__.py
@@ -1,5 +1,6 @@
 from .models.base import *
 from .models.users import *
 from .models.guilds import *
+from .models.groups import *
 from .exceptions import *
 from .fetch import StatusCodes, fetch, fetch_typed

716d1357bf0d6549ce43460787b9b7ae9b8291d2
Julien Kmec <me@julien.dev>
2024-02-03 15:57:43 -0500

    export utils

--- bloxlink_lib/__init__.py
+++ bloxlink_lib/__init__.py
@@ -4,4 +4,5 @@ from .models.guilds import *
 from .models.groups import *
 from .exceptions import *
 from .models.binds import *
+from .utils import *
 from .fetch import StatusCodes, fetch, fetch_typed

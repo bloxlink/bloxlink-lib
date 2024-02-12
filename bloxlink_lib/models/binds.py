c6b6b2998ad496f76ff4fd87632a678de36d7db7
Julien Kmec <me@julien.dev>
2024-02-11 21:48:21 -0500

    fix

--- bloxlink_lib/models/binds.py
+++ bloxlink_lib/models/binds.py
@@ -301,7 +301,7 @@ class GuildBind(BaseModel):
         content = self._content
 
         return (
-            f"- _{self._prefix}{' ' if content else ''}{f'**{content}** ' if content else ''}receive the "
+            f"- _{self._prefix}{' ' if content else ''}{f'**{content}** ' if content else ''} receive the "
             f"role{'s' if len(self.roles) > 1  else ''} {role_mentions}"
             f"{'' if len(self.remove_roles) == 0 else f', and have these roles removed: {remove_role_mentions}'}_"
         )

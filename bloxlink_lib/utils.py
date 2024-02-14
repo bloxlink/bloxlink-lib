baa5508e89f44a0d67ba509fd80b3998d669f88a
Julien Kmec <me@julien.dev>
2024-02-13 20:27:24 -0500

    fix

--- bloxlink_lib/utils.py
+++ bloxlink_lib/utils.py
@@ -4,7 +4,17 @@ def find[T](predicate: Callable, iterable: Iterable[T]) -> T | None:
     """Finds the first element in an iterable that matches the predicate."""
 
     for element in iterable:
-        if predicate(element):
-            return element
+        try:
+            iter(element)
+
+            if isinstance(element, str):
+                raise TypeError
+
+        except TypeError:
+            if predicate(element):
+                return element
+        else:
+            if predicate(*element):
+                return element
 
     return None

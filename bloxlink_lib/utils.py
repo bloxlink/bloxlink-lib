9e5e905bddadb237b18f8bd95573d84dfa946ae8
Julien Kmec <me@julien.dev>
2024-02-23 00:30:10 -0500

    check for Iterable instead

--- bloxlink_lib/utils.py
+++ bloxlink_lib/utils.py
@@ -14,13 +14,7 @@ def find[T](predicate: Callable, iterable: Iterable[T]) -> T | None:
     """Finds the first element in an iterable that matches the predicate."""
 
     for element in iterable:
-        try:
-            next(iter(element))
-
-            if isinstance(element, str):
-                raise TypeError
-
-        except TypeError:
+        if isinstance(element, Iterable) and not isinstance(element, str):
             if predicate(element):
                 return element
         else:

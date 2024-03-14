5c11b7468e0743ba630ed48901b809b5f75bbda1
Nub <38899321+One-Nub@users.noreply.github.com>
2024-03-14 19:49:40 +0000

    Add bind migration method

--- bloxlink_lib/models/binds.py
+++ bloxlink_lib/models/binds.py
@@ -499,6 +499,10 @@ async def get_binds(
     """
 
     guild_id = str(guild_id)
+
+    # Migrate any old bindings before we get the current binds.
+    await migrate_old_binds(guild_id)
+
     guild_data = await database.fetch_guild_data(
         guild_id,
         "binds",
@@ -546,46 +550,6 @@ async def get_binds(
         if category
         else guild_data.binds
     )
-
-    # Convert and save old bindings in the new format
-    # if not guild_data.converted_binds and (
-    #     guild_data.groupIDs is not None or guild_data.roleBinds is not None
-    # ):
-    #     old_binds = []
-
-    #     if guild_data.groupIDs:
-    #         old_binds.extend(convert_v3_binds_to_v4(guild_data.groupIDs, "group"))
-
-    #     if guild_data.roleBinds:
-    #         gamepasses = guild_data.roleBinds.get("gamePasses")
-    #         if gamepasses:
-    #             old_binds.extend(convert_v3_binds_to_v4(gamepasses, "gamepass"))
-
-    #         assets = guild_data.roleBinds.get("assets")
-    #         if assets:
-    #             old_binds.extend(convert_v3_binds_to_v4(assets, "asset"))
-
-    #         badges = guild_data.roleBinds.get("badges")
-    #         if badges:
-    #             old_binds.extend(convert_v3_binds_to_v4(badges, "badge"))
-
-    #         group_ranks = guild_data.roleBinds.get("groups")
-    #         if group_ranks:
-    #             old_binds.extend(convert_v3_binds_to_v4(group_ranks, "group"))
-
-    #     if old_binds:
-    #         # Prevent duplicates from being made. Can't use sets because dicts aren't hashable
-    #         guild_data.binds.extend(bind for bind in old_binds if bind not in guild_data.binds)
-
-    #         await update_guild_data(guild_id, binds=guild_data.binds, converted_binds=True)
-    #         guild_data.converted_binds = True
-
-    # if POP_OLD_BINDS and guild_data.converted_binds:
-    #     await update_guild_data(guild_id, groupIDs=None, roleBinds=None, converted_binds=None)
-
-    # return [
-    #     GuildBind(**bind) for bind in guild_data.binds
-    # ]
 
 
 async def get_nickname_template(guild_id, potential_binds: list[GuildBind]) -> tuple[str, GuildBind | None]:
@@ -752,71 +716,69 @@ async def parse_template(
     return template
 
 
-# def convert_v3_binds_to_v4(items: dict, bind_type: str) -> list:
-#     """Convert old bindings to the new bind format.
+async def migrate_old_binds(guild_id: str):
+    """Migrates binds from the V3 structure to V4 and saves them to the database.
 
-#     Args:
-#         items (dict): The bindings to convert.
-#         bind_type (ValidBindType): Type of bind that is being made.
+    If POP_OLD_BINDS is true, the old binds will be removed from the database.
+    """
+    guild_data = await database.fetch_guild_data(
+        guild_id,
+        "binds",
+        "roleBinds",
+        "groupIDs",
+        "converted_binds",
+    )
 
-#     Returns:
-#         list: The binds in the new format.
-#     """
-#     output = []
+    # Remove old binds and dip, but only if we've migrated already.
+    if POP_OLD_BINDS and guild_data.converted_binds:
+        await database.update_guild_data(guild_id, groupIDs=None, roleBinds=None, converted_binds=None)
+        return
 
-#     for bind_id, data in items.items():
-#         group_rank_binding = data.get("binds") or data.get("ranges")
+    # We've already migrated, don't try again.
+    if guild_data.converted_binds:
+        return
 
-#         if bind_type != "group" or not group_rank_binding:
-#             bind_data = {
-#                 "roles": data.get("roles"),
-#                 "removeRoles": data.get("removeRoles"),
-#                 "nickname": data.get("nickname"),
-#                 "bind": {"type": bind_type, "id": int(bind_id)},
-#             }
-#             output.append(bind_data)
-#             continue
+    migrated_binds = []
+    group_id_binds = guild_data.groupIDs or {}
+    rolebinds = guild_data.roleBinds or {}
 
-#         # group rank bindings
-#         if data.get("binds"):
-#             for rank_id, sub_data in data["binds"].items():
-#                 bind_data = {}
+    # Stop early if there's nothing to do.
+    if not group_id_binds and not rolebinds:
+        return
 
-#                 bind_data["bind"] = {"type": bind_type, "id": int(bind_id)}
-#                 bind_data["roles"] = sub_data.get("roles")
-#                 bind_data["nickname"] = sub_data.get("nickname")
-#                 bind_data["removeRoles"] = sub_data.get("removeRoles")
+    for group_id, bind_data in group_id_binds.items():
+        migrated_binds.append(GuildBind.from_V3("groupIDs", group_id, bind_data))
 
-#                 # Convert to an int if possible beforehand.
-#                 try:
-#                     rank_id = int(rank_id)
-#                 except ValueError:
-#                     pass
+    for bind_type, bindings in rolebinds.items():
+        match bind_type:
+            case "assets" | "badges" | "gamePasses":
+                # Assets, badges, gamePasses
+                for entity_id, bind_data in bindings.items():
+                    migrated_binds.append(GuildBind.from_V3(bind_type, entity_id, bind_data))
 
-#                 if rank_id == "all":
-#                     bind_data["bind"]["everyone"] = True
-#                 elif rank_id == 0:
-#                     bind_data["bind"]["guest"] = True
-#                 elif rank_id < 0:
-#                     bind_data["bind"]["min"] = abs(rank_id)
-#                 else:
-#                     bind_data["bind"]["roleset"] = rank_id
+                continue
 
-#                 output.append(bind_data)
+            case "groups":
+                # Single rank bindings + everyone/all + negative binds.
+                if bindings.get("binds"):
+                    for rank_id, criteria_data in bind_data["binds"].items():
+                        migrated_binds.append(
+                            GuildBind.from_V3_group_rolebind("roleset", group_id, {rank_id: criteria_data})
+                        )
 
-#         # group rank ranges
-#         if data.get("ranges"):
-#             for range_item in data["ranges"]:
-#                 bind_data = {}
+                # Ranges
+                if bindings.get("ranges"):
+                    for criteria_data in bind_data["ranges"]:
+                        migrated_binds.append(
+                            GuildBind.from_V3_group_rolebind("range", group_id, criteria_data)
+                        )
 
-#                 bind_data["bind"] = {"type": bind_type, "id": int(bind_id)}
-#                 bind_data["roles"] = range_item.get("roles")
-#                 bind_data["nickname"] = range_item.get("nickname")
-#                 bind_data["removeRoles"] = range_item.get("removeRoles")
+    if migrated_binds:
+        # TODO: Check for duplicates?
+        guild_data.binds.extend(migrated_binds)
 
-#                 bind_data["bind"]["min"] = int(range_item.get("low"))
-#                 bind_data["bind"]["max"] = int(range_item.get("high"))
-
-#                 output.append(bind_data)
-
-#     return output
+        await database.update_guild_data(
+            guild_id,
+            binds=[b.model_dump(exclude_unset=True, by_alias=True) for b in guild_data.binds],
+            converted_binds=True,
+        )

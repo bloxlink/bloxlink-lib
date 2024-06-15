6eef601174b3e5087af2ae623da18c7f814ddd3e
Julien Kmec <me@julien.dev>
2024-06-15 16:16:22 -0400

    use dict for mongo options

--- bloxlink_lib/database.py
+++ bloxlink_lib/database.py
@@ -25,6 +25,8 @@ def connect_database():
     global mongo # pylint: disable=global-statement
     global redis # pylint: disable=global-statement
 
+    mongo_options: dict[str, str | int] = {}
+
     if CONFIG.MONGO_CA_FILE:
         ca_file = exists("cert.crt")
 
@@ -32,13 +34,17 @@ def connect_database():
             with open("src/cert.crt", "w") as f:
                 f.write(CONFIG.MONGO_CA_FILE)
 
-    mongo = AsyncIOMotorClient(
-        host=CONFIG.MONGO_HOST or CONFIG.MONGO_URL,
-        port=int(CONFIG.MONGO_PORT) if CONFIG.MONGO_PORT else None,
-        username=CONFIG.MONGO_USER,
-        password=CONFIG.MONGO_PASSWORD,
-        tlsCAFile="src/cert.crt" if CONFIG.MONGO_CA_FILE else None
-    )
+        mongo_options["tlsCAFile"] = "src/cert.crt"
+
+    if CONFIG.MONGO_URL:
+        mongo_options["host"] = CONFIG.MONGO_URL
+    else:
+        mongo_options["host"] = CONFIG.MONGO_HOST
+        mongo_options["port"] = int(CONFIG.MONGO_PORT)
+        mongo_options["username"] = CONFIG.MONGO_USER
+        mongo_options["password"] = CONFIG.MONGO_PASSWORD
+
+    mongo = AsyncIOMotorClient(**mongo_options)
     mongo.get_io_loop = asyncio.get_running_loop
 
     if CONFIG.REDIS_URL:

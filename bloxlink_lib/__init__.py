695e2e72a699c93e252f9a17812f08c1ae691e9a
Julien Kmec <me@julien.dev>
2024-07-05 18:28:07 -0400

    bug: import base assets

--- bloxlink_lib/__init__.py
+++ bloxlink_lib/__init__.py
@@ -6,6 +6,7 @@ from .models.guilds import *
 from .models.groups import *
 from .models.badges import *
 from .models.gamepasses import *
+from .models.base_assets import *
 from .models.assets import *
 from .models.binds import *
 from .exceptions import *

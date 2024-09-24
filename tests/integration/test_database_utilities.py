add1e672c11d1e30138d0530991a97b678378714
Julien Kmec <me@julien.dev>
2024-09-23 21:01:06 -0700

    set strict to true for test

--- tests/integration/test_database_utilities.py
+++ tests/integration/test_database_utilities.py
@@ -3,10 +3,8 @@ from bloxlink_lib import database
 from pydantic import ValidationError
 
 
-
 class TestUpdatingGuildData:
     """Tests for converting V3 whole group binds to V4."""
-
 
     @pytest.mark.parametrize("test_input", [
         "sadasda",
@@ -18,12 +16,12 @@ class TestUpdatingGuildData:
         assert (await database.fetch_guild_data(1, "verifiedRoleName")).verifiedRoleName == test_input
 
     @pytest.mark.parametrize("test_input", [
-        # 5,
-        # 0,
-        pytest.param(-1, marks=pytest.mark.xfail)
+        5,
+        0,
+        -1
     ])
-    @pytest.mark.xfail(raises=ValidationError) 
-    async def test_update_guild_data_fails(self, test_input, start_docker_services, wait_for_redis):
+    @pytest.mark.xfail(strict=True, raises=ValidationError)
+    async def test_update_guild_data_invalid_input_fails(self, test_input, start_docker_services, wait_for_redis):
         await database.update_guild_data(1, verifiedRoleName=test_input)
 
         assert (await database.fetch_guild_data(1, "verifiedRoleName")).verifiedRoleName == test_input
